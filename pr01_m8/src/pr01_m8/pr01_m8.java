9d1ebb6abf7d62d043a91b18eee30a0cd2776fac
Sergio <Sergio@Sergio>
2016-03-20 13:59:37 +0100

    versio 0.2 del projecte

---
+++ pr01_m8/src/pr01_m8/pr01_m8.java
@@ -0,0 +1,34 @@
+
+ package pr01_m8; 
+import java.io.*; 
+public class pr01_m8 { 
+public static void main(String[] args) throws IOException { 
+File f = new File("fitxer.html"); 
+try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) { 
+bw.write("<html>");bw.newLine(); 
+bw.write(" <head>");bw.newLine(); 
+bw.write(" <title>");bw.newLine(); 
+bw.write(" Nova p&agrave;gina web"); 
+bw.newLine(); 
+bw.write(" </title>"); 
+bw.newLine(); 
+bw.write(" </head>"); 
+bw.newLine(); 
+bw.write(" <body>"); 
+bw.newLine(); 
+bw.write(" Nova p&agrave;gina web"); 
+bw.newLine(); 
+bw.write(" </body>"); 
+bw.newLine(); 
+bw.write("</html>"); 
+bw.write(" <h1>DAW2 m08uf4pr2</h1>"); 
+bw.newLine(); 
+bw.newLine(); 
+bw.close(); 
+} 
+} 
+} 
+
+    
+    
+
